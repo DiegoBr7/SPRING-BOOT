e125f9ab268065f8125a1ed3d5b16e0233fccab4
Diego <88857178+DiegoBr7@users.noreply.github.com>
2024-10-16 13:02:11 -0300

    Add files via upload

---
+++ Cap3 SPRING BOOT/SPRING BOOT/contatos/contatos/src/main/java/br/com/fiap/contatos/controller/HelloController.java
@@ -0,0 +1,22 @@
+package br.com.fiap.contatos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class HelloController
+
+{
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/ola")
+    public String ola(){
+        return "Ola MILTON QUANDO É QUE VOCE VEM NO BRASIL ";
+    }
+}
